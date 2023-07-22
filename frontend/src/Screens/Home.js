ab90721d3c080e37a15014fd21dc174bac6a82f6
Pratik Nimje <91190547+Pratik-A-N@users.noreply.github.com>
2023-07-22 22:25:21 +0530

    df

--- frontend/src/Screens/Home.js
+++ frontend/src/Screens/Home.js
@@ -5,7 +5,7 @@ import Peer from "simple-peer";
 import videoIcon from "../Assets/video.png";
 import AudioIcon from '../Assets/microphone.png'
 
-const socket = io.connect("http://localhost:5000");
+const socket = io.connect("https://peer-to-peer-backend.onrender.com:5000");
 
 function Home() {
   const [me, setme] = useState("");
